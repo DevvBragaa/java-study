9188d4134c1fe94432640e7dc8d31874480f628c
BragaDev <bragahenriquedev@gmail.com>
2025-05-25 19:15:59 -0300

    feat(project): Cria projeto

---
+++ body-calculator/src/main/java/com/imc/body_calculator/BodyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.imc.body_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BodyCalculatorApplication.class, args);
+	}
+
+}
---
+++ body-calculator/src/test/java/com/imc/body_calculator/BodyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imc.body_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
